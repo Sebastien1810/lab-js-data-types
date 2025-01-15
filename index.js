5089c5c89c14838a474bf31915b4f3c6c904be59
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-01-15 17:26:09 +0100

    individual la seb

--- index.js
+++ index.js
@@ -8,51 +8,43 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const newString = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
-
-
-
+console.log(newString);
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
 const part1 = "java";
-const part2 = "script";
+const part2 = " script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+// part1.slice(-1).toLocaleUpperCase();
+let part1bis = part1.slice(0, -1);
+let result1 = part1bis + part1.slice(-1).toUpperCase();
+let part2bis = part2.slice(0, -1);
+let result2 = part2bis + part2.slice(-1).toUpperCase();
 
+let result = result1.trim() + result2.trim();
 
 // Print the cameLtaiL-formatted string
-
-
-
-
+console.log(result);
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
-
 // Calculate the tip (15% of the bill total)
-
-
+let tipAmount = (84 * 15) / 100;
 // Print out the tipAmount
-
-
-
-
+console.log(tipAmount);
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
-
+let randomNumber = Math.floor(Math.random() * 10) + 1;
 // Print the generated random number
-
-
-
+console.log(Math.floor(Math.random() * 10) + 1);
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -62,15 +54,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+false;
 const expression2 = a || b;
-
+true;
 const expression3 = !a && b;
-
+false;
 const expression4 = !(a && b);
-
+true;
 const expression5 = !a || !b;
-
+true;
 const expression6 = !(a || b);
-
+false;
 const expression7 = a && a;
+true;
